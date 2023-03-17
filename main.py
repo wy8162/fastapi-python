4b6c993bfa1132dc8ca7b41e32434059f2fd608a
Yang Wang <wy8162@gmail.com>
2023-03-16 23:32:17 -0400

    port 80

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +12,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
   return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+  uvicorn.run(app, host="0.0.0.0", port=80)
