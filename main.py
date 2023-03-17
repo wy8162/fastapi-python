30dcf4fb9292ffeb5baa59bfe5ab91219511db32
Yang Wang <wy8162@gmail.com>
2023-03-17 17:54:07 -0400

    changes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
+books = {}
+
 app = FastAPI()
+
+
+class Book(BaseModel):
+  title: str
+  author: str
+  isbn: str
 
 
 @app.get("/")
@@ -12,6 +21,27 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
   return {"message": f"Hello {name}"}
+
+
+@app.get("/hello/{name}/age/{age}")
+async def say_hello(name: str, age: int):
+  return {"message": f"Hello {name}, you are {age} years old"}
+
+
+@app.post("/books")
+async def create_book(book: Book):
+  books[book.isbn] = book
+  return {"message": f"Book {book.title} has been created"}
+
+
+@app.get("/books/{isbn}")
+async def get_books(isbn: str):
+
+  book = books[isbn]
+  if book:
+    return {"message": book}
+  else:
+    return {"message": "Book not found"}
 
 
 if __name__ == "__main__":
